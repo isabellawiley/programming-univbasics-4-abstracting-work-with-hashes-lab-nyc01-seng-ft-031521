ef8c43acf2f0f5dcf04a54f295b1de1b828177f6
Isabella Wiley <wileyisabella@gmail.com>
2021-03-09 05:27:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -14,6 +14,8 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key] = NIL
     hash[key] = 1
+    hash[key]
+  else
+    hash[key]
   end
-  hash[key]
 end
